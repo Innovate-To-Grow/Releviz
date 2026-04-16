28f709761dba337c0b5d95b563cff3a27b4c29c7
Prachi Trivedi <prachi.a.trivedi@gmail.com>
2026-04-15 18:43:20 -0700

    restore sign-in and sign-up pages

--- frontend/app/sign-in/[[...sign-in]]/page.js
+++ frontend/app/sign-in/[[...sign-in]]/page.js
@@ -1,5 +1,16 @@
 import { SignIn } from "@clerk/nextjs";
 
-export default function Page() {
-  return <SignIn />;
+export const metadata = { title: "Sign In - Releviz" };
+
+export default function SignInPage() {
+  return (
+    <div style={{
+      minHeight: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    }}>
+      <SignIn />
+    </div>
+  );
 }
--- frontend/app/sign-up/[[...sign-up]]/page.js
+++ frontend/app/sign-up/[[...sign-up]]/page.js
@@ -1,5 +1,16 @@
 import { SignUp } from "@clerk/nextjs";
 
-export default function Page() {
-  return <SignUp />;
+export const metadata = { title: "Sign Up - Releviz" };
+
+export default function SignUpPage() {
+  return (
+    <div style={{
+      minHeight: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    }}>
+      <SignUp />
+    </div>
+  );
 }
